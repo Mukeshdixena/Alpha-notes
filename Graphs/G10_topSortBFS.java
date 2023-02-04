f52df2c43e515a8621f9da47d534064d1298f81c
Mukeshdixena <mukesh.dixena2002@gmail.com>
2023-02-04 10:56:42 +0530

    git commit

--- Graphs/G10_topSortBFS.java
+++ Graphs/G10_topSortBFS.java
@@ -31,28 +31,37 @@ public class G10_topSortBFS {
 
     }
 
-    static void topSort(ArrayList<Edge>[] graph) {
-        boolean vis[] = new boolean[graph.length];
-        Stack<Integer> s = new Stack<>();
+    static void calcIndeg(ArrayList<Edge> graph[], int indeg[]){
         for (int i = 0; i < graph.length; i++) {
-            if (!vis[i]) {
-                topSortUtil(graph, vis, s, i);
+            for (int j = 0; j < graph[i].size(); j++) {
+                Edge e = graph[i].get(j);
+                indeg[e.dst]++;
             }
-        }
-        while (!s.isEmpty()) {
-            System.out.print(s.pop() + " ");
         }
     }
 
-    static void topSortUtil(ArrayList<Edge>[] graph, boolean vis[], Stack<Integer> s, int curr) {
-        vis[curr] = true;
-        for (int i = 0; i < graph[curr].size(); i++) {
-            Edge e = graph[curr].get(i);
-            if (!vis[e.dst]) {
-                topSortUtil(graph, vis, s, e.dst);
+    static void topSort(ArrayList<Edge>[] graph) {
+        int indeg[] = new int[graph.length];
+        calcIndeg(graph, indeg);
+        Queue<Integer> q =  new LinkedList<>();
+        for (int i = 0; i < indeg.length; i++) {
+            if(indeg[i] == 0){
+                q.add(i);
             }
         }
-        s.push(curr);
+
+        while(!q.isEmpty()){
+            int curr = q.remove();
+            System.out.print(curr + " ");
+
+            for (int i = 0; i < graph[curr].size(); i++) {
+                Edge e = graph[curr].get(i);
+                indeg[e.dst]--;
+                if(indeg[e.dst] == 0){
+                    q.add(e.dst);
+                }
+            }
+        }
     }
 
     @SuppressWarnings("unchecked")
