e16ff74ea3588c0c1d8ae8279dcd0b285dd1bd1f
Mukeshdixena <mukesh.dixena2002@gmail.com>
2023-02-07 17:08:30 +0530

    git push

---
+++ Array/A01_linearSearch.java
@@ -0,0 +1,13 @@
+package Array;
+
+public class A01_linearSearch {
+
+    static int linearSearch(int arr[], int k) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == k) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
---
+++ Array/A02_largest_in_array.java
@@ -0,0 +1,11 @@
+package Array;
+
+public class A02_largest_in_array {
+    static int largestNum(int arr[]) {
+        int max = arr[0]; // it can be Integer.MAX_VALUE
+        for (int i = 1; i < arr.length; i++) {
+            max = max < arr[i] ? arr[i] : max;
+        }
+        return max;
+    }
+}
