8fba93d4a5c54fc775e0a8617b05b0ca91454e5f
Mukeshdixena <mukesh.dixena2002@gmail.com>
2023-02-19 09:57:37 +0530

    git push

---
+++ DynamicProgramming/DP14_wildCard.java
@@ -0,0 +1,45 @@
+package DynamicProgramming;
+
+public class DP14_wildCard {
+    static void tabulatiion(String s, String p) {
+        boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
+
+        for (int i = 0; i < dp.length; i++) {
+            dp[i][0] = false;
+        }
+        for (int j = 1; j < dp[0].length; j++) {
+            if (p.charAt(j-1) == '*') {
+                dp[0][j] = dp[0][j - 1];
+            }
+        }
+        for (int i = 1; i < dp.length; i++) {
+            for (int j = 1; j < dp[0].length; j++) {
+                if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
+                    dp[i][j] = true;
+                } else if (p.charAt(j - 1) == '*') {
+                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                } else {
+                    dp[i][j] = false;
+                }
+            }
+        }
+        printDp(dp);
+    }
+
+    static void printDp(boolean dp[][]) {
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (dp[i][j]) {
+                    System.out.print(1 + " ");
+                } else {
+                    System.out.print(0 + " ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        tabulatiion("dp", "**?p*");
+    }
+}
--- Temp/Temp6.java
+++ Temp/Temp6.java
@@ -1,60 +1,23 @@
 package Temp;
 
-import java.util.ArrayList;
-import java.util.Scanner;
+import java.math.BigInteger;
 
 public class Temp6 {
 
-    static int factorial(int n) {
-        return n * (n - 1);
-    }
-
-    static boolean isPrime(int n) {
-        boolean ans = true;
-
-        for (int i = 2; i < Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                ans = false;
-            }
-        }
-
-        return ans;
-    }
-
-    static void prime(int n) {
+    public static BigInteger fibonacciModified(int t1, int t2, int n) {
+        // Write your code here
+        BigInteger a = BigInteger.valueOf(t1);
+        BigInteger b = BigInteger.valueOf(t2);
+        BigInteger c = new BigInteger("0");
         for (int i = 0; i < n; i++) {
-
+            c = a.add(b.multiply(b));
+            a = b;
+            b = c;
         }
-    }
-
-    static ArrayList<Integer> findFactor(int n) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            if (n % i == 0) {
-                ans.add(i);
-            }
-        }
-        return ans;
-    }
-
-    static ArrayList<Integer> findPrimeFactor(int n) {
-        ArrayList<Integer> ans = new ArrayList<>();
-        for (int i = 2; i <= n; i++) {
-            while (n % i == 0) {
-                ans.add(i);
-                n /= i;
-            }
-        }
-        return ans;
+        return c;
     }
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-
-        ArrayList<Integer> ans = findFactor(n);
-        System.out.println(ans);
-
-        sc.close();
+        
     }
 }
