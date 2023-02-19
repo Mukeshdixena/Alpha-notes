fa7882ee4db42aeaa1cac4115ca6b869e0992ce5
Mukeshdixena <mukesh.dixena2002@gmail.com>
2023-02-19 10:05:13 +0530

    git push

--- DynamicProgramming/DP14_wildCard.java
+++ DynamicProgramming/DP14_wildCard.java
@@ -3,8 +3,8 @@ package DynamicProgramming;
 public class DP14_wildCard {
     static void tabulatiion(String s, String p) {
         boolean dp[][] = new boolean[s.length() + 1][p.length() + 1];
-
-        for (int i = 0; i < dp.length; i++) {
+        dp[0][0] = true;
+        for (int i = 1; i < dp.length; i++) {
             dp[i][0] = false;
         }
         for (int j = 1; j < dp[0].length; j++) {
@@ -15,9 +15,9 @@ public class DP14_wildCard {
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[0].length; j++) {
                 if (s.charAt(i - 1) == p.charAt(j - 1) || p.charAt(j - 1) == '?') {
-                    dp[i][j] = true;
+                    dp[i][j] = dp[i-1][j-1];
                 } else if (p.charAt(j - 1) == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
+                    dp[i][j] = dp[i][j-1] || dp[i-1][j];
                 } else {
                     dp[i][j] = false;
                 }
