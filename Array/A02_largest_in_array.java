3e9beb86b6992d24098f459df1d81a5aca68906c
Mukesh Dixena <97972429+Mukeshdixena@users.noreply.github.com>
2023-03-12 13:10:54 +0530

    Update A02_largest_in_array.java

--- Array/A02_largest_in_array.java
+++ Array/A02_largest_in_array.java
@@ -2,7 +2,7 @@ package Array;
 
 public class A02_largest_in_array {
     static int largestNum(int arr[]) {
-        int max = arr[0]; // it can be Integer.MAX_VALUE
+        int max = arr[0]; // it can be Integer.MIN_VALUE
         for (int i = 1; i < arr.length; i++) {
             max = max < arr[i] ? arr[i] : max;
         }
