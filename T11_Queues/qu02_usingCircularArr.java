9371064736e6c92f0ac67bfccc1575f415b80825
Mukesh <mukeshdixena123@gmail.com>
2023-04-14 15:04:09 +0530

    git commit

--- T11_Queues/qu02_usingCircularArr.java
+++ T11_Queues/qu02_usingCircularArr.java
@@ -1,65 +1,64 @@
 package T11_Queues;
-// package queuessss;
 
-// public class qu02_usingCircularArr {
+public class qu02_usingCircularArr {
 
-//     public static void main(String[] args) {
-//         Queue qu = new Queue(5);
-//         qu.add(1);
-//         qu.add(2);
-//         qu.add(3);
-//         qu.add(4);
-//         while (!qu.isEmpty()) {
-//             System.out.println(qu.remove());
-//         }
-//     }
-// }
+    public static void main(String[] args) {
+        Queue qu = new Queue(5);
+        qu.add(1);
+        qu.add(2);
+        qu.add(3);
+        qu.add(4);
+        while (!qu.isEmpty()) {
+            System.out.println(qu.remove());
+        }
+    }
+}
 
-// class Queue {
-//     static int arr[];
-//     static int size;
-//     static int rear;
-//     static int front;
+class Queue {
+    static int arr[];
+    static int size;
+    static int rear;
+    static int front;
 
-//     Queue(int n) {
-//         arr = new int[n];
-//         size = n;
-//         rear = -1;
-//         front = -1;
-//     }
+    Queue(int n) {
+        arr = new int[n];
+        size = n;
+        rear = -1;
+        front = -1;
+    }
 
-//     boolean isEmpty() {
-//         return rear == -1 && front == -1;
-//     }
+    boolean isEmpty() {
+        return rear == -1 && front == -1;
+    }
 
-//     boolean isfull() {
-//         return rear + 1 == front;
-//     }
+    boolean isfull() {
+        return rear + 1 == front;
+    }
 
-//     void add(int data) {
-//         if (isfull()) {
-//             System.out.println("queue is full");
-//             return;
-//         }
-//         rear = (rear + 1) % size;
-//         arr[rear] = data;
-//     }
+    void add(int data) {
+        if (isfull()) {
+            System.out.println("queue is full");
+            return;
+        }
+        rear = (rear + 1) % size;
+        arr[rear] = data;
+    }
 
-//     int remove() {
-//         if (isEmpty()) {
-//             System.out.println("que is empty");
-//             return -1;
-//         }
-//         int val = arr[front];
-//         front = (front + 1) % size;
-//         return val;
-//     }
+    int remove() {
+        if (isEmpty()) {
+            System.out.println("que is empty");
+            return -1;
+        }
+        int val = arr[front];
+        front = (front + 1) % size;
+        return val;
+    }
 
-//     int peek() {
-//         if (isEmpty()) {
-//             System.out.println("que is empty");
-//             return -1;
-//         }
-//         return arr[0];
-//     }
-// }
+    int peek() {
+        if (isEmpty()) {
+            System.out.println("que is empty");
+            return -1;
+        }
+        return arr[0];
+    }
+}
